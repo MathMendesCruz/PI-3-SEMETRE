03d950bbc4466eaecd623d906834f6d9d90a68c4
MathMendesCruz <matheusoliveiramendes123@gmail.com>
2025-12-10 00:10:50 -0300

    Remove: Deletar completamente todas as referências ao SQLite do projeto

--- migrate.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Script para executar migrations
-// Acesse: https://elegancejoias.store/migrate.php
-
-echo "Executando migrations...\n\n";
-$output = shell_exec("php artisan migrate --force 2>&1");
-echo "<pre>$output</pre>";
-echo "\n\nPronto! <a href='/'>Voltar ao site</a>";
-?>
