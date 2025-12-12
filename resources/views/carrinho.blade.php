4a210242a944f5c9613a29ecd26c875524df8184
MathMendesCruz <matheusoliveiramendes123@gmail.com>
2025-12-11 21:49:36 -0300

    .

--- resources/views/carrinho.blade.php
+++ resources/views/carrinho.blade.php
@@ -48,9 +48,9 @@
                         </div>
                         <div class="cart-item-controls">
                             <div class="quantity-selector">
-                                <button class="quantity-btn decrease-qty" onclick="updateCartItem({{ $item['id'] }}, Math.max(1, {{ $item['quantity'] }} - 1))">-</button>
+                                <button class="quantity-btn decrease-qty" onclick="changeQty(this, {{ $item['id'] }}, -1)">-</button>
                                 <span class="quantity-value">{{ $item['quantity'] }}</span>
-                                <button class="quantity-btn increase-qty" onclick="updateCartItem({{ $item['id'] }}, {{ $item['quantity'] }} + 1)">+</button>
+                                <button class="quantity-btn increase-qty" onclick="changeQty(this, {{ $item['id'] }}, 1)">+</button>
                             </div>
                             <button class="delete-item-btn" onclick="removeFromCart({{ $item['id'] }})">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 011 1v6a1 1 0 11-2 0V9a1 1 0 011-1zm4 0a1 1 0 011 1v6a1 1 0 11-2 0V9a1 1 0 011-1z" clip-rule="evenodd" /></svg>
@@ -113,4 +113,16 @@
 </div>
 
 @include('partials.contact')
+@endsection
+
+@section('extra-scripts')
+<script>
+// Ajusta quantidade e sincroniza com o backend
+function changeQty(btn, productId, delta) {
+    const valueEl = btn.parentElement.querySelector('.quantity-value');
+    let qty = parseInt(valueEl.textContent) || 1;
+    qty = Math.max(1, qty + delta);
+    updateCartItem(productId, qty);
+}
+</script>
 @endsection
